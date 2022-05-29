fea7be9b32775f509a0838fa5fd95faf94c7abdf
lee-plus-plus <leezhuoming@qq.com>
2022-05-29 21:49:09 +0800

    fixed test4

--- test/test3.cpp
+++ test/test3.cpp
@@ -9,14 +9,17 @@
 using namespace std;
 
 int main() {
+  string str;
+  cin >> str;
   sly::utils::Log::SetLogLevel(sly::utils::Log::kError);
-  sly::core::lexical::RegEx r1("[0-9]+([Ee][+-]?[0-9]+)(f|F|l|L)?");
+  sly::core::lexical::RegEx r1(str);
+  auto ret = sly::core::lexical::DfaModel::Merge({r1.GetDfaModel()});
+
   // sly::core::lexical::RegEx c_section_begin("%{");
   // auto end_token = Token::Terminator("EOF_FLAG");
   // auto any_token = Token::Terminator("any");
   // auto c_section_begin_token = Token::Terminator("c_section_begin");
 
-  // auto ret = sly::core::lexical::DfaModel::Merge({r1.GetDfaModel(), c_section_begin.GetDfaModel()});
   // sly::runtime::Stream2TokenPipe s2ppl(ret.first, ret.second, {
   //   any_token, c_section_begin_token
   // }, end_token);
